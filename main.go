a27044c40b928392a20500231a960b9430d2017c
sagarhani <sagarhani33@gmail.com>
2019-03-05 23:17:11 +0530

    Save to file

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// cards := newDeck()
 
@@ -10,5 +8,6 @@ func main() {
 	// remainingCards.print()
 
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	//fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
