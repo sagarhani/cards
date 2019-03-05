b0540773e469b95390ea6c1a1a3b7f24a0872342
sagarhani <sagarhani33@gmail.com>
2019-03-05 23:04:05 +0530

    Converting Deck type to String

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-func main() {
-	cards := newDeck()
+import "fmt"
 
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+func main() {
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
